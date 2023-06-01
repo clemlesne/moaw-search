27cde491196662533a9ea01076c9f68a86d7ed3b
Clémence Lesné <clemence@lesne.pro>
2023-06-01 18:16:20 +0200

    Compat: Automatically add CSS compat

--- src/search-ui/vite.config.js
+++ src/search-ui/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import autoprefixer from 'autoprefixer'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer()
+      ],
+    }
+  }
 })
